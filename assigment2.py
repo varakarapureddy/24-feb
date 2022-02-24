d029980387c4f50cea28b2c56a5a8c7e70d47523
varakarapureddy <99267924+varakarapureddy@users.noreply.github.com>
2022-02-24 22:36:57 +0530

    done assigment2

---
+++ assigment2.py
@@ -0,0 +1,16 @@
+def function(x):
+    array = []
+
+    for num in x:
+        if num <= 1:
+            continue
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
+            array.append(num)
+
+    return len(array)
+z = [4, 5, 6, 7, 8, 9, 10]
+print(function(z))
